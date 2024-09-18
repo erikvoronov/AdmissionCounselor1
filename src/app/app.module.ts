61b2088d30f912327ec921486d18932399d5195b
erikvoronov <134633499+erikvoronov@users.noreply.github.com>
2024-09-18 13:13:06 -0400

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+/*
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SchoolChooserComponent } from './school-chooser/school-chooser.component';
+import { StudentChooserComponent } from './student-chooser/student-chooser.component';
+import { SortPipe } from './sort.pipe';
+import { ChooserComponent } from './chooser/chooser.component';
+import {routing} from "./app.routing";
+import { StudentViewComponent } from './student-view/student-view.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SchoolChooserComponent,
+    StudentChooserComponent,
+    SortPipe,
+    ChooserComponent,
+    StudentViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    routing,
+    FormsModule
+  ],
+  providers: [
+    StudentChooserComponent,
+    SchoolChooserComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+*/
